c01a9553d1ceaf19902517b7e327c8152a5085dc
koki-sys <soraisu7@gmail.com>
2022-01-19 23:21:02 +0900

    ルーティングの追加

---
+++ src/Route.tsx
@@ -0,0 +1,16 @@
+import { Navigate, useRoutes } from "react-router-dom"
+
+export const RootRouter = () => {
+    return useRoutes([
+        {
+            // element: <HomeLayout />,
+
+            children: [
+                { path: "/", element: <div>Home</div> },
+                { path: "/about", element: <div>About</div> },
+                { path: "/profile", element: <div>Profile</div> },
+            ],
+        },
+        { path: "*", element: <Navigate to="/404" /> },
+    ])
+}
