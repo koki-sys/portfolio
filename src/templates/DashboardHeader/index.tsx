2da863d7003ee92f5e2192f3b10f57d5fc4d9467
koki-sys <soraisu7@gmail.com>
2022-01-21 16:02:23 +0900

    headerレイアウトの修正

--- src/templates/DashboardHeader/index.tsx
+++ src/templates/DashboardHeader/index.tsx
@@ -1,3 +1,30 @@
+import "templates/DashboardHeader/style.css"
+
 export const DashboardHeader = () => {
-    return <div></div>
+    return (
+        <nav className="flex items-center justify-between flex-wrap header-bg p-4 shadow">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <span className="text-xl tracking-tight header-text">
+                    KokiSys Portfolio
+                </span>
+            </div>
+            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto"></div>
+            <div>
+                <div className="text-sm lg:flex-grow">
+                    <a
+                        href="/works"
+                        className="block mt-4 lg:inline-block lg:mt-0 header-text hover:text-white mr-4"
+                    >
+                        Works
+                    </a>
+                    <a
+                        href="/about"
+                        className="block mt-4 lg:inline-block lg:mt-0 header-text hover:text-white"
+                    >
+                        About
+                    </a>
+                </div>
+            </div>
+        </nav>
+    )
 }
