5d8313d946940a1aab4f3a43b71fcf7c61203336
koki-sys <soraisu7@gmail.com>
2022-01-21 14:00:49 +0900

    MUIの削除

--- src/templates/DashboardHeader/index.tsx
+++ src/templates/DashboardHeader/index.tsx
@@ -1,24 +1,3 @@
-import { AppBar, Toolbar, Button, Box, Typography } from "@material-ui/core"
-
-import useStyles from "templates/DashboardHeader/style"
-
 export const DashboardHeader = () => {
-    const styles = useStyles()
-
-    return (
-        <AppBar className={styles.appbar}>
-            <Toolbar>
-                <Typography variant="h6" className={styles.title}>
-                    Koki-Sys Portfolio
-                </Typography>
-                <Box sx={{ flexGrow: 1 }}></Box>
-                <Button className={styles.btn}>
-                    <p>Works</p>
-                </Button>
-                <Button className={styles.btn}>
-                    <p>About</p>
-                </Button>
-            </Toolbar>
-        </AppBar>
-    )
+    return <div></div>
 }
